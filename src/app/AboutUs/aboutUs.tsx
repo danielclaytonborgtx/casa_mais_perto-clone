3d6ea9383aeadd7ef66d98bfd7e62174a967fcf4
danielclayton <danielclaitul@gmail.com>
2024-10-01 13:36:39 -0300

    commit 7

--- src/app/AboutUs/aboutUs.tsx
+++ src/app/AboutUs/aboutUs.tsx
@@ -6,10 +6,10 @@ import { styles } from './styles';
 const AboutUs = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sobre Nós</Text>
+      <Text style={styles.title}>Casa Mais Perto 🏡</Text>
       <Text style={styles.paragraph}>
-        Nosso aplicativo busca excelência no ramo imobiliário, oferecendo aos nossos usuários as melhores
-        opções de casas e apartamentos. Nosso compromisso é com a qualidade e a satisfação dos nossos clientes.
+          Encontre seu novo lar de forma rápida e prática com o Casa Mais Perto! Nossa plataforma foi desenvolvida para facilitar a busca pelo imóvel ideal, conectando você a casas e apartamentos disponíveis ao seu redor. Com uma interface intuitiva e integrada ao mapa, você pode visualizar todas as opções próximas à sua localização, garantindo que você descubra o imóvel perfeito na região que você mais ama.
+          Encontrar seu novo lar ficou mais fácil e rápido.
       </Text>
     </View>
   );
---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+// src/services/api.ts
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://192.168.100.6:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
