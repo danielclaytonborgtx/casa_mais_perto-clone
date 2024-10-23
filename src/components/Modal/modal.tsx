45a9c34c7e6a63b146dbab896946ea17c157bbba
danielclayton <danielclaitul@gmail.com>
2024-10-23 14:14:00 -0300

    commit 12

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Modal as RNModal, View, Text, TouchableOpacity, Animated } from 'react-native';
 import { styles } from './styles';
 import { User as IconUser } from 'lucide-react-native';
@@ -10,36 +10,40 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ visible, onClose }) => {
-  const slideAnim = useRef(new Animated.Value(-300)).current; // Começa fora da tela à esquerda
+  const slideAnim = useRef(new Animated.Value(-300)).current; 
+  const [showModal, setShowModal] = useState(visible);
   const router = useRouter();
 
   useEffect(() => {
     if (visible) {
-      // Animação para abrir o modal
+      
+      setShowModal(true);
       Animated.timing(slideAnim, {
-        toValue: 0, // Anima para a posição 0 (visível)
+        toValue: 0, 
         duration: 300,
         useNativeDriver: true,
       }).start();
     } else {
-      // Animação para fechar o modal
+      
       Animated.timing(slideAnim, {
-        toValue: -300, // Anima para fora da tela à esquerda
+        toValue: -300, 
         duration: 300,
         useNativeDriver: true,
-      }).start();
+      }).start(() => {
+        setShowModal(false); 
+      });
     }
   }, [visible]);
 
   const handleNavigation = (route: string) => {
-    router.push(route as any); // Usando type assertion
-    onClose(); // Fecha o modal
+    router.push(route as any); 
+    onClose(); 
   };
 
   return (
     <RNModal
       transparent={true}
-      visible={visible}
+      visible={showModal} 
       onRequestClose={onClose}
     >
       <TouchableOpacity style={styles.container} onPress={onClose}>
